e5bf8481173139162fb682312d9d4a04d1dbbe77
GUUDIN <86815140+GUUDIN@users.noreply.github.com>
2023-09-28 08:59:51 -0300

    Finished Ex3 & updated comments

--- Pratica1/Pratica1_ex2.asm
+++ Pratica1/Pratica1_ex2.asm
@@ -1,24 +1,24 @@
-org 	00h	; Origem em 00h
+	org 	00h	; Origem em 00h
 
-beginning:	; Inicializar o programa com uma label
+beginning:		; Inicializa o programa com uma label
+	MOV	A,#02h	; Move de forma imediata o valor 2 em decimal para o ACC.
+	MOV 	B,#03h	; Move de forma imediata o valor 3 em decimal para B
+	MOV 	R0,#07h	; Move para um endereco de memoria qualquer o valor imediato 7 em decimal
+	ADD	A, R0	; Soma o conteudo do endereco de memoria escolhido na linha anterior com ACC.
+	DEC	A	; Decrementa 3 unidades de ACC
+	DEC	A
+	DEC	A
+	INC 	B	; Incrementa 1 unidade em B
+	SUBB 	A,B	; Subtrai A por B
+	MUL 	AB	; Multiplica A por B
+	INC 	B	; Incrementa 2 unidades em B
+	INC 	B
+	DIV 	AB	; Divide A por B
+	MOV 	R0, A	; Armazena os conteudos de A e B em dois enderecos de memoria quaisquer na RAM.
+	MOV 	R1, B
+JMP 	beginning	; Salta para label declarada no inicio
+end			; Encerra o programa.
 
-MOV	A,#02h	; Mover de forma imediata o valor 2 em decimal para o ACC.
-MOV 	B,#03h	; Mover de forma imediata o valor 3 em decimal para B
-MOV 	R0,#07h	; Mover para um endereco de memoria qualquer o valor imediato 7 em decimal
-ADD	A, R0	; Somar o conteudo do endereco de memoria escolhido na linha anterior com ACC.
-DEC	A	; Decrementar 3 unidades de ACC
-DEC	A
-DEC	A
-INC 	B	; Incrementar 1 unidade em B
-SUBB 	A,B	; Subtrair A por B
-MUL 	AB	; Multiplicar A por B
-INC 	B	; Incrementar 2 unidades em B
-INC 	B
-DIV 	AB	; Dividir A por B
-MOV 	R0, A	; Armazenar os conteudos de A e B em dois enderecos de memoria quaisquer na RAM.
-MOV 	R1, B
-JMP 	beginning
-		; Saltar para label declarada no inicio
-end		; Encerrar o programa.
-
-;Resposta a questao: O bit menos significativo do PSW representa a paridade do numero de bits no acumulador, no caso do numero 4 temos 0100b, por isso o psw apresenta o numero 1 (quantidade impar de numeros 1). De maneira similar, o numero 3 (0011b) possui uma quantidade par de numeros 1 e por isso o PSW apresenta um 0 no seu bit menos significativo.
+;Resposta a questao: O bit menos significativo do PSW representa a paridade do numero de bits no acumulador,
+;no caso do numero 4 temos 0100b, por isso o psw apresenta o numero 1 (quantidade impar de numeros 1).
+;De maneira similar, o numero 3 (0011b) possui uma quantidade par de numeros 1 e por isso o PSW apresenta um 0 no seu bit menos significativo. 
--- Pratica1/Pratica1_ex3.asm
+++ Pratica1/Pratica1_ex3.asm
@@ -1,14 +1,16 @@
-	org 00h;Criar um novo programa
-;Colocar a origem no endereço 00h
-;Inicializar o programa com uma label
-;Mover de forma imediata para o ACC e para B dois valores em binário (8 bits), distintos (evitar escolher valores em que todos os dígitos são iguais: 11111111 ou 00000000, por ex., e escolher de forma que pelo menos 1 bit seja igual na mesma posição entre os dois valores. Por ex.: ambos compartilham bit 1 no dígito menos significativo ou em alguma outra posição).
-;Realizar AND lógico entre A e B
-;Rotacionar A à direita em 2 bits.
-;Realizar o complemento de A
-;Rotacionar A à esquerda em 2 bits.
-;Realizar OR lógico entre A e B
-;Realizar XOR entre A e B
-;Realizar SWAP de A;
-;Saltar para a label inicial
-;Encerrar o programa.
-;Em “bit field information” no simulador EdSim51 (onde geralmente é exibido PSW no formato binário), colocar ACC no lugar de PSW.
+	org  00h	; Coloca a origem no endereço 00h
+
+beginning: 		; Inicializa o programa com uma label
+	MOV  A,#0011b	; Move (forma imediata) 0000 0011b para o ACC
+	MOV  B,#1001b	; Move (forma imediata) 0000 1001b para o ACC
+	ANL  A,B	; Realiza AND lógico entre A e B
+	RR   A		; Rotaciona A à direita em 2 bits.
+	RR   A			
+	CPL  A		; Realiza o complemento de A
+	RL   A		; Rotaciona A à esquerda em 2 bits.
+	RL   A
+	ORL  A,B	; Realiza OR lógico entre A e B
+	XRL  A,B	; Realiza XOR entre A e B
+	SWAP A		; Realiza SWAP de A;
+JMP beginning		; Salta para a label inicial
+end			; Encerra o programa.
